55116b62d478bcb1046acfbe286068d0311d4c64
brett-white90 <151653400+brett-white90@users.noreply.github.com>
2024-11-29 19:41:52 -0600

    feat: add initial extension files and successful webpack build

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    popup: './src/popup/popup.ts',
+    background: './src/background/background.ts',
+    content: './src/content/content.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]/[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/manifest.json', to: './manifest.json' },
+        { from: './src/popup/popup.html', to: './popup/popup.html' },
+        { from: './src/styles', to: './styles' },
+      ],
+    }),
+  ],
+};
